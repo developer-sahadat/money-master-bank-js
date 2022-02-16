29df1fab462340e879069fc48fdc0f40eb6428f4
developer-sahadat <mdridoysikder862@gmail.com>
2022-02-17 03:41:17 +0600

    js complated

--- js/app.js
+++ js/app.js
@@ -1,24 +1,49 @@
 
 
 /***********expenses total ammount****************/
-function expenses(){
+function expensesAmmount(){
+
+    //input tages
     const foodInput=document.getElementById('food-input');
     const rentInput=document.getElementById('rent-input');
     const clothesInput=document.getElementById('clothes-input');
-    const totalExpenses=parseFloat(foodInput.value)+parseFloat(rentInput.value)+parseFloat(clothesInput.value);
 
+    if(foodInput.value<0 || rentInput.value<0 || clothesInput.value<0){
+        return 'Please type the number';
+    }
+    
+    const totalExpenses=parseInt(foodInput.value)+parseInt(rentInput.value)+parseInt(clothesInput.value);
     return totalExpenses;
+
 }
 
 /*********************Calculate button code********************/
 
 document.getElementById('Calculate-btn').addEventListener('click', function(){
-    document.getElementById('total-expenses').innerText=expenses();
-    const incomeInput=document.getElementById('income-input');
-    const balance=parseFloat(incomeInput.value)-expenses();
-    document.getElementById('balance').innerText=balance;
+
+    let expenses=expensesAmmount();
+    const incomeInput=document.getElementById('income-input').value;
+    if(typeof expenses=='number' && expenses>0 && typeof parseInt(incomeInput)=='number' && parseInt(incomeInput)>0){
+        document.getElementById('total-expenses').innerText=expenses;
+        const balance=parseInt(incomeInput)-expenses;
+        document.getElementById('balance').innerText=balance;
+    }else{
+        alert('Please type the number')
+    }
 })
 
 
+
+
 /************************save button code***************************/
-document.getElementById('save-btn')
+document.getElementById('save-btn').addEventListener('click', function(){
+    const incomeInput=document.getElementById('income-input');
+    const savingInput=document.getElementById('saving-input');
+
+    const savingAmount=(parseFloat(incomeInput.value)*parseFloat(savingInput.value))/100;
+    document.getElementById('saving-amount').innerText=savingAmount;
+
+    const balance=document.getElementById('balance').innerText;
+    document.getElementById('remaining-balance').innerText=parseFloat(balance)-savingAmount;
+
+})
